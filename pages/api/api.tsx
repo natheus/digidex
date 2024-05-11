72614c0b19b3f2b3caa9db05517c2d1f39c0e898
Vinicius <39202981+Vinicius-Brito-Costa@users.noreply.github.com>
2024-05-11 17:11:56 -0300

    [FIX] Fetch data from new URL

--- pages/api/api.tsx
+++ pages/api/api.tsx
@@ -2,16 +2,16 @@ import axios from "axios";
 
 function baseApi(page: number) {
   return axios.get(
-    `https://www.digi-api.com/api/v1/digimon?pageSize=100&page=${page}`
+    `https://digi-api.com/api/v1/digimon?pageSize=100&page=${page}`
   );
 }
 
 function createDigiInfosInstance(id: string) {
-  return axios.get(`https://www.digi-api.com/api/v1/digimon/${id}`);
+  return axios.get(`https://digi-api.com/api/v1/digimon/${id}`);
 }
 
 function getDigiByName(name: string) {
-  return axios.get(`https://www.digi-api.com/api/v1/digimon/${name}`);
+  return axios.get(`https://digi-api.com/api/v1/digimon/${name}`);
 }
 
 export default { baseApi, createDigiInfosInstance, getDigiByName };
